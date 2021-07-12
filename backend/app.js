4a0b4dad66d5bfddd078d0593ce4dc9ceaa5093b
Andrey Azovkin <adm.az-an@yandex.ru>
2021-07-12 18:20:21 +0300

    -15- спринт#8

--- backend/app.js
+++ backend/app.js
@@ -16,6 +16,15 @@ const NotFoundError = require('./errors/not-found-err');
 const { PORT = 3000 } = process.env;
 const app = express();
 
+
+mongoose.connect('mongodb://localhost:27017/mestodb', {
+  useNewUrlParser: true,
+  useCreateIndex: true,
+  useFindAndModify: false,
+  useUnifiedTopology: true,
+});
+
+
 app.use(cookieParser());
 app.use(express.json());
 
@@ -91,11 +100,4 @@ app.use((err, req, res, next) => {
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.listen(PORT);
-
-mongoose.connect('mongodb://localhost:27017/mestodb', {
-  useNewUrlParser: true,
-  useCreateIndex: true,
-  useFindAndModify: false,
-  useUnifiedTopology: true,
-});
 

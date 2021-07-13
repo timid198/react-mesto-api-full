c320e25f76ad0cd7d4744889848c1485d6e752b6
Andrey Azovkin <adm.az-an@yandex.ru>
2021-07-13 21:56:38 +0300

    -15- спринт #18-2

--- backend/app.js
+++ backend/app.js
@@ -87,6 +87,7 @@ app.use((err, req, res, next) => {
   next(err);
 });
 app.use((err, req, res, next) => {
+  console.log(err);
   const { statusCode = 500, message } = err;
   res
     .status(statusCode)

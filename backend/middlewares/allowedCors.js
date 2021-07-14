2bf05a2360e665c971287d7c8c9ee5f67b9d9590
Andrey Azovkin <adm.az-an@yandex.ru>
2021-07-14 16:32:15 +0300

    -15- спринт #19-1

--- backend/middlewares/allowedCors.js
+++ backend/middlewares/allowedCors.js
@@ -10,7 +10,7 @@ module.exports = (req, res, next) => {
   const { method } = req;
   const requestHeaders = req.headers['access-control-request-headers'];
   if (allowedCors.includes(origin)) {
-    res.header('Access-Control-Allow-Origin', `${origin}`);
+    res.header('Access-Control-Allow-Origin', origin);
     res.header('Access-Control-Allow-Credentials', true);
   }
   if (method === 'OPTIONS') {

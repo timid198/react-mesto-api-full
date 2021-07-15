cb7ac41afcdf75fba6dc1b0f82762a34e1957104
Andrey Azovkin <adm.az-an@yandex.ru>
2021-07-15 16:59:23 +0300

    -15- спринт #20-7

--- backend/middlewares/allowedCors.js
+++ backend/middlewares/allowedCors.js
@@ -1,16 +1,18 @@
 const allowedCors = [
   'https://azannik.nomoredomains.rocks',
   'http://azannik.nomoredomains.rocks',
+  'http://localhost:3000',
+  'http://localhost:3001',
 ];
 
-const DEFAULT_ALLOWED_METHODS = 'PUT,PATCH,DELETE';
+const DEFAULT_ALLOWED_METHODS = 'GET,HEAD,PUT,PATCH,POST,DELETE';
 
 module.exports = (req, res, next) => {
   const { origin } = req.headers;
   const { method } = req;
   const requestHeaders = req.headers['access-control-request-headers'];
   if (allowedCors.includes(origin)) {
-    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Origin', origin);
     res.header('Access-Control-Allow-Credentials', true);
   }
   if (method === 'OPTIONS') {

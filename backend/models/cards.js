29beecce5d36c54d769f6668387871760443f675
Andrey Azovkin <adm.az-an@yandex.ru>
2021-07-16 22:44:18 +0300

    -15- спринт #22-11

--- backend/models/cards.js
+++ backend/models/cards.js
@@ -12,7 +12,7 @@ const cardSchema = new mongoose.Schema({
     required: true,
     validate: {
       validator: function validate(v) {
-        return /^(https|http):\/\/(www\.)?[A-Za-z0-9-]*\.[A-Za-z0-9]{2}[A-Za-z0-9-._~:\\/?#[\]@!$&'()*+,;=]*#?$/.test(v);
+        return /^(https|http):\/\/(www\.)?[A-Za-z0-9-]*\.[A-Za-z0-9]{2}[A-Za-z0-9-._~:\\/?#[\]@!$&'()*+,;=]*/.test(v);
       },
     },
   },

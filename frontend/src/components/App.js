96d0ff361c846bfea9767d2a23c44dfe97ca2757
Andrey Azovkin <adm.az-an@yandex.ru>
2021-07-17 16:01:06 +0300

    -15- спринт #22-50

--- frontend/src/components/App.js
+++ frontend/src/components/App.js
@@ -100,7 +100,7 @@ function handleUpdateAvatar(props) {
   
   function handleCardLike(card) {
     setLoading(true)
-    const isLiked = card.likes.some(i => i._id === currentUser._id);
+    const isLiked = card.likes.some(i => i === currentUser._id);
     api.changeCardsLikes(card._id, isLiked)
     .then((newCard) => {
       setCards((state) => state.map((c) => c._id === card._id ? newCard : c));})

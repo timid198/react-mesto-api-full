60c323b337a5b1ccd27854e84fc56365b9327040
Andrey Azovkin <adm.az-an@yandex.ru>
2021-07-17 11:30:35 +0300

    -15- спринт #22-9

--- frontend/src/components/Card.js
+++ frontend/src/components/Card.js
@@ -12,10 +12,10 @@ function Card({ card, cardClick, onCardDelete, onCardLike }) {
     `element__trash ${(isOwn || isCardOwn) ? 'element__trash_set' : 'element__trash_unset'}`
   ); 
 
-  const isLiked = card.likes.some(i => i === currentUser._id);
-  const isCardLiked = card.likes._id.some(i => i === currentUser._id);
-
-  console.log(`при рендере: ${isCardLiked}, при лайке: ${isLiked}, выражение: ${isLiked || isCardLiked}`);
+  const isLiked = card.likes.some(i => i.includes(currentUser._id));
+  // const isCardLiked = card.likes._id.some(i => i === currentUser._id);
+  console.log(`при лайке: ${isLiked}`);
+  // console.log(`при рендере: ${isCardLiked}, при лайке: ${isLiked}, выражение: ${isLiked || isCardLiked}`);
 
   const cardLikeButtonClassName = (
     `element__title-like ${isLiked ? 'element__title-like_unset' : 'element__title-like_set'}`

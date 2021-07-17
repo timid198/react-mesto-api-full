ce67b174a5131a1ee13b334c3baaed86c59027b7
Andrey Azovkin <adm.az-an@yandex.ru>
2021-07-17 16:38:56 +0300

    -15- спринт #22-62

--- frontend/src/utils/api.js
+++ frontend/src/utils/api.js
@@ -67,7 +67,7 @@ class Api {
     }
 
     changeCardsLikes(_id, isLike) {
-        const status = isLike ? 'DELETE' : 'PUT';
+        const status = isLike ?  'PUT' : 'DELETE';
         return fetch(`${this._address}/cards/${_id}/likes`, {
             method: `${status}`,
             credentials: 'include',
